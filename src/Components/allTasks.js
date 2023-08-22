5bc85f8d04c2d2cf58005420976040fab3a1f7a3
adrian1lara <adrianlara203@gmail.com>
2023-08-22 00:08:41 -0500

    update allTasks file

--- src/Components/allTasks.js
+++ src/Components/allTasks.js
@@ -2,14 +2,12 @@ import Task from "./task";
 import { taskbox, tasks } from "..";
 import { format } from "date-fns";
 
-
 function allTasks() {
     const div = document.createElement('div');
     tasks.map(task => {
         div.appendChild(task.addNewTask());
     });
 
-    console.log(tasks)
     return div;
 }
 
