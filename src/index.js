87c69b855a9f942fbd5aa2c14c80ba5d24a8f01e
adrian1lara <adrianlara203@gmail.com>
2023-08-21 22:37:43 -0500

    update file logistic

--- src/index.js
+++ src/index.js
@@ -3,86 +3,11 @@ import allTasks from "./Components/allTasks";
 import impTasks from "./Components/impTasks";
 import todayTasks from "./Components/todayTasks";
 import Task from "./Components/task";
-import { parse, format } from 'date-fns';
+import { parse, format, parseISO } from 'date-fns';
 
 export let tasks = [];
 export const taskbox = document.querySelector('.task');
-
-const savedTasks = localStorage.getItem('tasks');
-if (savedTasks) {
-    tasks = JSON.parse(savedTasks);
-    renderTasks(allTasks)
-}
-
-
-const taskForm = document.getElementById('task-form');
-const overlay = document.createElement('div');
-overlay.className = 'overlay';
-
 const addButton = document.getElementById('addButton');
-addButton.addEventListener('click', () => {
-    overlay.appendChild(taskForm);
-    document.body.appendChild(overlay);
-})
-
-let editingTask = null;
-
-taskForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    if (editingTask) {
-        // Edit existing task
-        editingTask.title = taskForm.title.value;
-        editingTask.description = taskForm.description.value;
-        editingTask.dueDate = parse(taskForm.dueDate.value, 'yyyy-MM-dd', new Date());
-        editingTask.priority = taskForm.priority.value;
-
-        // Update task in the DOM
-        editingTask.updateTask();
-
-        editingTask = null;
-    } else {
-        // Add new task
-        const title = taskForm.title.value;
-        const description = taskForm.description.value;
-        const dueDate = parse(taskForm.dueDate.value, 'yyyy-MM-dd', new Date());
-        const priority = taskForm.priority.value;
-
-        const newTask = new Task(title, description, dueDate, priority);
-        tasks.push(newTask);
-
-        taskbox.appendChild(newTask.addNewTask());
-
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-
-    taskForm.reset();
-    overlay.parentNode.removeChild(overlay);
-});
-
-taskbox.addEventListener('click', (e) => {
-    const editButton = e.target.closest('.edit-button');
-    if (editButton) {
-        // Get the task element containing the edit button
-        const taskElement = editButton.closest('.task');
-
-        // Find the corresponding task instance
-        const taskIndex = Array.from(taskbox.children).indexOf(taskElement);
-        const task = tasks[taskIndex];
-
-        // Fill the form fields with task's data for editing
-        taskForm.title.value = task.title;
-        taskForm.description.value = task.description;
-        taskForm.dueDate.value = format(task.dueDate, 'yyyy-MM-dd');
-        taskForm.priority.value = task.priority;
-
-        editingTask = task;
-
-        // Show the form for editing
-        overlay.appendChild(taskForm);
-        document.body.appendChild(overlay);
-    }
-});
 
 const allTask = document.getElementById('allTasks');
 allTask.addEventListener('click', () => {
@@ -99,6 +24,42 @@ const impTask = document.getElementById('impTasks');
 impTask.addEventListener('click', () => {
     renderTasks(impTasks);
 })
+
+addButton.addEventListener('click', () => {
+    const taskForm = document.getElementById('task-form');
+
+    taskForm.style.display = 'flex';
+    addButton.style.display = 'none';
+
+    taskForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const title = document.getElementById('title');
+        const description = document.getElementById('description');
+        const dueDate = document.getElementById('dueDate');
+        const priority = document.getElementById('priority');
+
+        const date = parseISO(dueDate.value);
+        format(date, 'dd/MM/yyyy');
+        
+        const task = new Task(title.value, description.value, date, priority.value);
+        console.log(task);
+
+        tasks.push(task);
+        renderTasks(allTasks);
+
+        taskForm.style.display = 'none';
+        addButton.style.display = 'block';
+
+        taskForm.reset();
+
+    })
+
+})
+
+
+
+
+
 
 
 
