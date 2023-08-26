304f30fcbb0ec216d3420a9c1fe5e919d85dfa61
adrian1lara <adrianlara203@gmail.com>
2023-08-25 22:57:32 -0500

    new addButtonClickListener function added and fix the bug of the update and createTask

--- src/Components/todayTasks.js
+++ src/Components/todayTasks.js
@@ -1,14 +1,28 @@
+import '../style.css';
 import Task from "./task";
 import { format, isSameDay, parseISO } from "date-fns";
-import { createNewTask, dueDateInput, myTasks } from "./allTasks";
-//import { taskbox} from "..";
-import { removeTask,  } from "./allTasks";
-import { titleInput, descriptionInput, priorityInput, addButtonClickListener } from "./allTasks";
-import { submitButton } from "./allTasks";
-import { taskForm } from "./allTasks";
+import { myTasks } from "./allTasks";
+import { removeTask } from "./allTasks";
+import { showAllTaskForm } from './allTasks';
 
 const taskContainer = document.getElementById('TodayTask');
+const todayTaskForm = document.getElementById('todayTask-form');
+const titleInput = document.getElementById('todayTitle');
+const descriptionInput = document.getElementById('todayDescription');
+const priorityInput = document.getElementById('todayPriority');
+const submitTodayButton = document.getElementById('addTodayTask');
+export const showTodayForm = document.getElementById('showTodayForm');
 
+let selectedTodayTask = null;
+
+
+const addButtonClickListener = () => {
+    showAllTaskForm.style.display = 'none';
+    showTodayForm.style.display = 'flex';
+    showTodayForm.addEventListener('click', () => {
+        todayTaskForm.style.display = 'flex';
+    })
+}
 
 const createTodayTask = () => {
     const newTitle = titleInput.value;
@@ -21,11 +35,19 @@ const createTodayTask = () => {
         return;
     }
 
-
-    const newTask = new Task(newTitle, newDescription, format(dueDateValue, 'yyyy-MM-dd'), newPriority);
-    myTasks.push(newTask);
-    displayTodayTasks();
-
+    if (selectedTodayTask) {
+        selectedTodayTask.title = newTitle;
+        selectedTodayTask.description = newDescription;
+        selectedTodayTask.date = format(dueDateValue, 'yyyy-MM-dd');
+        selectedTodayTask.priority = newPriority;
+        selectedTodayTask = null;
+        displayTodayTasks();
+    
+    } else {
+        const newTask = new Task(newTitle, newDescription, format(dueDateValue, 'yyyy-MM-dd'), newPriority);
+        myTasks.push(newTask);
+        displayTodayTasks();
+    }
 
     titleInput.value = '';
     descriptionInput.value = '';
@@ -33,6 +55,7 @@ const createTodayTask = () => {
 }
 
 const displayTodayTasks = () => {
+    taskContainer.innerHTML = '';
     const today = new Date()
     const todayTask = myTasks.filter((task) => {
         const taskDate = parseISO(task.date);
@@ -40,7 +63,6 @@ const displayTodayTasks = () => {
         return isSameDay(taskDate, today);
     })
 
-    taskContainer.innerHTML = '';
     todayTask.forEach((task) => {
         const div = document.createElement('div');
         const titleLabel = document.createElement('h2');
@@ -50,11 +72,14 @@ const displayTodayTasks = () => {
         const deleteButton = document.createElement('button');
         const editButton = document.createElement('button');
         
-
+        div.className = 'newTask-box';
         titleLabel.className = 'task-title';
         descriptionLabel.className = 'task-description';
         dueDateLabel.className = 'task-date';
         priorityLabel.className = 'task-priority';
+        deleteButton.className = 'task-delete';
+        editButton.className = 'task-edit';
+
 
         titleLabel.textContent = task.title;
         descriptionLabel.textContent = task.description;
@@ -65,10 +90,21 @@ const displayTodayTasks = () => {
 
         editButton.addEventListener('click', () => {
             console.log("Editar");
+            submitTodayButton.removeEventListener('click', createTodayTask);
+            submitTodayButton.addEventListener('click', updateTodayTask);
+            submitTodayButton.textContent = 'Update';
+
+            updateTodayTask(task);
+            todayTaskForm.style.display = 'flex';
+            submitTodayButton.removeEventListener('click', updateTodayTask);
+            submitTodayButton.addEventListener('click', createTodayTask);
         })
+
+        submitTodayButton.textContent = 'Add Task';
 
         deleteButton.addEventListener('click', () => {
             removeTask(task);
+            displayTodayTasks();
         })
 
         console.log(task);
@@ -83,16 +119,21 @@ const displayTodayTasks = () => {
         taskContainer.appendChild(div);
     })
 
-    taskForm.style.display = 'none';
+    todayTaskForm.style.display = 'none';
 }
 
-function todayTasks() {
+const updateTodayTask = (task) => {
+    selectedTodayTask = task;
+    titleInput.value = task.title;
+    descriptionInput.value = task.description;
+    priorityInput.value = task.priority;
+}
 
+
+function todayTasks() {
     taskContainer.style.display = 'block';
     addButtonClickListener();
-    dueDateInput.style.display = 'none';
-    submitButton.removeEventListener('click', createNewTask);
-    submitButton.addEventListener('click', createTodayTask);
+    submitTodayButton.addEventListener('click', createTodayTask);
     displayTodayTasks();
     return taskContainer;
 }
