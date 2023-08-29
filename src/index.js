169868ebb835d1c31dc20d3f0b646110b92e95eb
adrian1lara <adrianlara203@gmail.com>
2023-08-28 21:34:10 -0500

    border to what page you are

--- src/index.js
+++ src/index.js
@@ -7,17 +7,27 @@ export const taskbox = document.getElementById('task');
 
 const allTask = document.getElementById('allTasks');
 allTask.addEventListener('click', () => {
+    impTask.style.borderBottom = 'none';
+    todayTask.style.borderBottom = 'none';
+    allTask.style.borderBottom = '1px solid #fff';
     renderTasks(allTasks);
 })
 
 
 const todayTask = document.getElementById('todayTasks');
 todayTask.addEventListener('click', () => {
+    impTask.style.borderBottom = 'none';
+    allTask.style.borderBottom = 'none';
+    todayTask.style.borderBottom = '1px solid #fff';
+
     renderTasks(todayTasks);
 })
 
 const impTask = document.getElementById('impTasks');
 impTask.addEventListener('click', () => {
+    todayTask.style.borderBottom = 'none';
+    allTask.style.borderBottom = 'none';
+    impTask.style.borderBottom = '1px solid #fff';
     renderTasks(impTasks);
 })
 
